4a72ad685da26a4934058b63e7d97792a63b0f7f
Le-Roy <jongweleroy@yahoo.com>
2022-03-30 07:40:46 +0200

    created new file reception.js

---
+++ assets/js/reception/reception.js
@@ -0,0 +1,24 @@
+let nokObject = {
+	nok_dropdown: document.getElementById("nok-dropdown"),
+	nok_rel: document.getElementById("nok-rel"),
+	list: document.getElementById("rel-ol"),
+
+	nok_m: function()
+	{
+		nokObject.nok_rel.addEventListener("click", function(){
+			if(nokObject.nok_dropdown.style.display == ""){
+				nokObject.nok_dropdown.style.display = "block";
+				for(var i = 0; i < list.childElementCount; i++){
+					list.children[i].addEventListener("click", function(){
+						nokObject.nok_rel.value = this.innerText;
+						nokObject.nok_dropdown.style.display = "";
+					});
+				}
+			}else{
+				nokObject.nok_dropdown.style.display = "";
+			}
+		});
+	},
+}
+
+nokObject.nok_m();
