171f7d1f6fa460a7da0bdabe019f35b5b902efd6
Le-Roy <jongweleroy@yahoo.com>
2022-04-01 05:52:28 +0200

    modified

--- clinicdb.sql
+++ clinicdb.sql
@@ -16,6 +16,20 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `activity`
+--
+
+DROP TABLE IF EXISTS `activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `activity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `encounter` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `assesment`
 --
 
@@ -35,15 +49,6 @@ CREATE TABLE `assesment` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `assesment`
---
-
-LOCK TABLES `assesment` WRITE;
-/*!40000 ALTER TABLE `assesment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `assesment` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `company`
 --
 
@@ -58,20 +63,14 @@ CREATE TABLE `company` (
   `co_contact_a` varchar(255) NOT NULL,
   `co_contact_b` varchar(255) DEFAULT NULL,
   `co_email` varchar(255) NOT NULL,
+  `co_country` varchar(100) NOT NULL,
+  `co_state` varchar(100) NOT NULL,
+  `co_zipCode` varchar(100) NOT NULL,
   `co_address` text NOT NULL,
   `date_registered` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `company`
---
-
-LOCK TABLES `company` WRITE;
-/*!40000 ALTER TABLE `company` DISABLE KEYS */;
-/*!40000 ALTER TABLE `company` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `credentials`
@@ -85,21 +84,13 @@ CREATE TABLE `credentials` (
   `co_id` int(11) DEFAULT NULL,
   `role_id` int(11) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
+  `account_type` varchar(100) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `credentials`
---
-
-LOCK TABLES `credentials` WRITE;
-/*!40000 ALTER TABLE `credentials` DISABLE KEYS */;
-/*!40000 ALTER TABLE `credentials` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `doctorsnotes`
@@ -120,15 +111,6 @@ CREATE TABLE `doctorsnotes` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `doctorsnotes`
---
-
-LOCK TABLES `doctorsnotes` WRITE;
-/*!40000 ALTER TABLE `doctorsnotes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `doctorsnotes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `employee_details`
 --
 
@@ -143,18 +125,8 @@ CREATE TABLE `employee_details` (
   `start_date` date DEFAULT NULL,
   `dismis_date` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `employee_details`
---
-
-LOCK TABLES `employee_details` WRITE;
-/*!40000 ALTER TABLE `employee_details` DISABLE KEYS */;
-INSERT INTO `employee_details` VALUES (2,15,'receptionist','','2022-02-03',NULL),(4,17,'nurse','','2022-02-07',NULL),(5,18,'nurse','123-786','2022-03-17',NULL);
-/*!40000 ALTER TABLE `employee_details` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `investigations`
@@ -176,15 +148,6 @@ CREATE TABLE `investigations` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `investigations`
---
-
-LOCK TABLES `investigations` WRITE;
-/*!40000 ALTER TABLE `investigations` DISABLE KEYS */;
-/*!40000 ALTER TABLE `investigations` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `medical_conditions`
 --
 
@@ -197,17 +160,8 @@ CREATE TABLE `medical_conditions` (
   `pec` text DEFAULT NULL,
   `allergies` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `medical_conditions`
---
-
-LOCK TABLES `medical_conditions` WRITE;
-/*!40000 ALTER TABLE `medical_conditions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `medical_conditions` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `next_of_kin`
@@ -226,17 +180,8 @@ CREATE TABLE `next_of_kin` (
   `relationship` varchar(255) NOT NULL,
   `address` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `next_of_kin`
---
-
-LOCK TABLES `next_of_kin` WRITE;
-/*!40000 ALTER TABLE `next_of_kin` DISABLE KEYS */;
-/*!40000 ALTER TABLE `next_of_kin` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `patient`
@@ -260,17 +205,8 @@ CREATE TABLE `patient` (
   `address` text NOT NULL,
   `registered_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `patient`
---
-
-LOCK TABLES `patient` WRITE;
-/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
-/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `plan`
@@ -291,15 +227,6 @@ CREATE TABLE `plan` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `plan`
---
-
-LOCK TABLES `plan` WRITE;
-/*!40000 ALTER TABLE `plan` DISABLE KEYS */;
-/*!40000 ALTER TABLE `plan` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `prescription`
 --
 
@@ -318,15 +245,6 @@ CREATE TABLE `prescription` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `prescription`
---
-
-LOCK TABLES `prescription` WRITE;
-/*!40000 ALTER TABLE `prescription` DISABLE KEYS */;
-/*!40000 ALTER TABLE `prescription` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `roles_permissions`
 --
 
@@ -339,17 +257,8 @@ CREATE TABLE `roles_permissions` (
   `permissions` varchar(255) NOT NULL,
   `level` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `roles_permissions`
---
-
-LOCK TABLES `roles_permissions` WRITE;
-/*!40000 ALTER TABLE `roles_permissions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `roles_permissions` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -367,17 +276,8 @@ CREATE TABLE `user` (
   `contact` varchar(20) DEFAULT NULL,
   `address` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `vitals`
@@ -404,15 +304,6 @@ CREATE TABLE `vitals` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `vitals`
---
-
-LOCK TABLES `vitals` WRITE;
-/*!40000 ALTER TABLE `vitals` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vitals` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -423,4 +314,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-17  5:36:58
+-- Dump completed on 2022-03-31 14:33:35
